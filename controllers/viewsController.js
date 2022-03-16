496111c8cfbf6363617f5cbadf6f4aabd23f94bd
Saman Fathnazarian <saman.nazarian17@gmail.com>
2022-03-16 11:58:43 -0500

    implementted each tour  with server-side rendering using pug

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -15,8 +15,14 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user' //specifying these fields to show
   });
-};
+
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour: tour
+  });
+});
