60c0378160560c397073da30358505ad2f09070f
Saman Fathnazarian <saman.nazarian17@gmail.com>
2022-03-16 13:29:45 -0500

    added js file, which will be used to show a map, on the frontend

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,1 +1,8 @@
-console.log('hello from the client side');
+/* eslint-disable */
+
+// whatever we put into a data attribute like this (data-locations=''), will then get stored into the dataset property,
+// and in this case dataset.locations.
+
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+console.log(locations);
