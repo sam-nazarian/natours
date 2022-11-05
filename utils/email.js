a0780d0500308fb3c8db3ec7db41efe3079e4bfa
samanfathnazarian <saman.nazarian17@gmail.com>
2022-11-05 14:24:54 -0500

    Implemented building a complex email handler
    Refactored the email function in the email file into a class.

--- utils/email.js
+++ utils/email.js
@@ -1,11 +1,67 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Saman Fathnazarian <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //sendgrid
+      return 1;
+    }
+
+    return nodemailer.createTransport({
+      //using mailtrap for practise
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
+
+  // Send the actual email
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject
+    });
+
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.htmlToText(html)
+    };
+
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    return await this.send('welcome', 'Welcome to the Natours Family!'); //return exits the function, not necessorly in this case
+  }
+};
+
+////////////////////////////
+////////////////////////////
 /**
  * @param {object} options object with properties of 'email', 'subject', 'message'
  */
+/*
 const sendEmail = async (options) => {
   //1) Create a transporter
-
   const transporter = nodemailer.createTransport({
     //using mailtrap for practise
     host: process.env.EMAIL_HOST,
@@ -28,5 +84,5 @@ const sendEmail = async (options) => {
   // 3) Actually send the email
   await transporter.sendMail(mailOptions);
 };
-
 module.exports = sendEmail;
+*/
