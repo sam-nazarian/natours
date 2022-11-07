7d4dda14e9c11a25833343ccefa421ff0a99eff7
samanfathnazarian <saman.nazarian17@gmail.com>
2022-11-07 01:17:11 -0600

    Implemented modeling the bookings

---
+++ models/bookingModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Booking must belong to a Tour!']
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Booking must belong to a User!']
+  },
+  price: {
+    type: Number,
+    required: [true, 'Booking must have a price.']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  // incase the customer decides to pay in cash an administer is supposed to enter this data (booking done outside of Stripe)
+  paid: {
+    type: boolean,
+    default: true
+  }
+});
+
+bookingSchema.pre(/^find/, function(next) {
+  this.populate('user').populate({
+    path: 'tour',
+    select: 'name'
+  });
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
