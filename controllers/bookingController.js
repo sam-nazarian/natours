8a4654773fac795a2d6c5713364b194b3f6d2e50
samanfathnazarian <saman.nazarian17@gmail.com>
2022-11-07 23:40:34 -0600

    Finished the Bookings API
    Added all CRUD endpoints to the bookings router & controller.

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -52,3 +52,11 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
   res.redirect(req.originalUrl.split('?')[0]);
   // res.redirect('/'); // also works
 });
+
+//adding CRUD endpoints
+exports.getAllBookings = factory.getAll(Booking);
+exports.createBooking = factory.createOne(Booking);
+
+exports.getBooking = factory.getOne(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routs/bookingRoutes.js
+++ routs/bookingRoutes.js
@@ -4,6 +4,21 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router(); //can see parameter of other routers
 
+router.use(authController.protect);
+
 // console.log('HERE AT THE BOOKINGS');
 router.get('/checkout-session/:tourId', authController.protect, bookingController.getCheckoutSession);
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
+
 module.exports = router;
